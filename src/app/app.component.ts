10e09736e4409635372103e329ff0c1de1bd1f2b
tuyen.vv <tuyen.vv@boruapps.com>
2020-04-08 16:42:01 +0700

    #44804 - update FCM open and dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,63 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { FCM } from '@ionic-native/fcm/ngx';
+import {NavController, Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {FCM} from '@ionic-native/fcm/ngx';
 import {AppConfig} from './AppConfig';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private fcm: FCM,
-    public appConst: AppConfig,
-    private router: Router
-  ) {
-    this.initializeApp();
-  }
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private fcm: FCM,
+        public appConst: AppConfig,
+        private router: Router,
+        public navCtrl: NavController
+    ) {
+        this.initializeApp();
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-        // get FCM token
-        this.fcm.getToken().then(token => {
-            console.log(token);
-            this.appConst.setFCMToken(token);
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+            // get FCM token
+            this.fcm.getToken().then(token => {
+                console.log(token);
+                this.appConst.setFCMToken(token);
+            });
+
+            // ionic push notification example
+            this.fcm.onNotification().subscribe(data => {
+                console.log(data);
+                if (data.wasTapped) {
+                    console.log('Received in background');
+                    //this.router.navigate(['/tabs/notifications', {fragment: ''}]);
+                    this.navCtrl.navigateRoot('tabs/notifications');
+                } else {
+                    console.log('Received in foreground');
+                    //this.router.navigate(['/tabs/notifications', {fragment: ''}]);
+                    this.navCtrl.navigateRoot('tabs/notifications');
+                }
+            });
+
+            // refresh the FCM token
+            this.fcm.onTokenRefresh().subscribe(token => {
+                console.log(token);
+                this.appConst.setFCMToken(token);
+            });
+
+            // unsubscribe from a topic
+            // this.fcm.unsubscribeFromTopic('offers');
         });
-
-        // ionic push notification example
-        this.fcm.onNotification().subscribe(data => {
-            console.log(data);
-            if (data.wasTapped) {
-                console.log('Received in background');
-                this.router.navigate(['notifications']);
-            } else {
-                console.log('Received in foreground');
-                this.router.navigate(['notifications']);
-            }
-        });
-
-        // refresh the FCM token
-        this.fcm.onTokenRefresh().subscribe(token => {
-            console.log(token);
-            this.appConst.setFCMToken(token);
-        });
-
-        // unsubscribe from a topic
-        // this.fcm.unsubscribeFromTopic('offers');
-    });
-  }
+    }
 }
